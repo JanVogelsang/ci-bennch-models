d91caa657be030074afcc1577f842ff8f3058738
Jan Vogelsang <47158055+JanVogelsang@users.noreply.github.com>
2023-04-19 15:10:05 +0200

    Update hpc_benchmark.py

--- hpc_benchmark/hpc_benchmark.py
+++ hpc_benchmark/hpc_benchmark.py
@@ -166,7 +166,8 @@ brunel_params = {
     'mean_potential': 5.7,
     'sigma_potential': 7.2,
 
-    'delay': 1.5,  # synaptic delay, all connections(ms)
+    'delay': {dendritic_delay},  # synaptic dendritic delay, all connections(ms)
+    'axonal_delay': {axonal_delay},  # synaptic axonal delay, all connections(ms)
 
     # synaptic weight
     'JE': 0.14,  # peak of EPSP
@@ -175,7 +176,8 @@ brunel_params = {
     'g': -5.0,
 
     'stdp_params': {
-        'delay': 1.5,
+        'delay': {dendritic_delay},
+        'axonal_delay': {axonal_delay},
         'alpha': 0.0513,
         'lambda': 0.1,  # STDP step size
         'mu': 0.4,  # STDP weight dependence exponent(potentiation)
