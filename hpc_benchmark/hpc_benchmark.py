2d6ebd52b3ba263a966afb61e62de4aeb5bf555a
Jan Vogelsang <47158055+JanVogelsang@users.noreply.github.com>
2024-03-21 14:59:38 +0100

    Update hpc_benchmark.py

--- hpc_benchmark/hpc_benchmark.py
+++ hpc_benchmark/hpc_benchmark.py
@@ -413,6 +413,7 @@ def run_simulation():
         presim_steps += 1
 
     PreparationTime = time.time() - tic
+    time_simulate_presim = nest.kernel_status["time_simulate"]
     tic = time.time()
 
     for d in range(sim_steps):
@@ -450,8 +451,9 @@ def run_simulation():
          'init_memory_peak': init_memory_peak,
          'total_memory_peak': total_memory_peak,
          'average_rate': average_rate}
-    d.update(build_dict)
-    d.update(nest.kernel_status)
+    final_kernel_status = nest.kernel_status
+    final_kernel_status["time_simulate"] -= time_simulate_presim
+    d.update(final_kernel_status)
     print(d)
 
     nest.Cleanup()
