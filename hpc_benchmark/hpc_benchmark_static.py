2f395c0c09780d1dd65cb0c08f11e26d39ac1854
Jan Vogelsang <47158055+JanVogelsang@users.noreply.github.com>
2025-06-05 11:24:53 +0200

    Update hpc_benchmark_static.py

--- hpc_benchmark/hpc_benchmark_static.py
+++ hpc_benchmark/hpc_benchmark_static.py
@@ -211,8 +211,7 @@ def build_network():
                           'resolution': params['dt'],
                           'rng_seed': params['rng_seed'],
                           'overwrite_files': True,
-                          'keep_source_table': False,
-                          'num_targets_completed_before_forming_connections': params['num_targets_completed_before_forming_connections']})
+                          'keep_source_table': False})
 
     try:
         num_targets_completed_before_forming_connections = int(params['num_targets_completed_before_forming_connections'])
